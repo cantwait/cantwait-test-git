03b7958a45aa1d897bd3230e34e59b941fd3cc0c
cantwait <rafael.cadenas@admios-sa.com>
2014-07-25 21:31:36 -0500

    Added person id to console output

--- java-git-test/src/org/home/test/PersonaMain.java
+++ java-git-test/src/org/home/test/PersonaMain.java
@@ -5,7 +5,12 @@ public class PersonaMain {
 	public static void main(String[] args) {
 		System.out.println("THIS IS A USEFULL APP");
 		System.out.println("---------------------");
+		System.out.println("Id Persona: "+ getPersonId());
 		
+	}
+	
+	public static String getPersonId(){
+		return new Persona("1").getId();
 	}
 
 }
