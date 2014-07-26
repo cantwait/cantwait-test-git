95a55d1c167f6f4ee241683a7fcca61e34d35c90
cantwait <rafael.cadenas@admios-sa.com>
2014-07-25 21:29:24 -0500

    ADD COMMENT TO CONSOLE MESSAGE

--- java-git-test/src/org/home/test/PersonaMain.java
+++ java-git-test/src/org/home/test/PersonaMain.java
@@ -3,7 +3,7 @@ package org.home.test;
 public class PersonaMain {
 
 	public static void main(String[] args) {
-		System.out.println("THIS IS A USEFULL APP");
+		System.out.println("THIS IS A USEFULL APP FOR TESTING");
 		System.out.println("---------------------");
 		
 	}
