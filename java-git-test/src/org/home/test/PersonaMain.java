f04ea3017f73f84ed15cc94ea1ade8cdde3a0225
cantwait <rafael.cadenas@admios-sa.com>
2014-07-25 21:35:52 -0500

    Added property to person and method to main

--- java-git-test/src/org/home/test/PersonaMain.java
+++ java-git-test/src/org/home/test/PersonaMain.java
@@ -5,7 +5,10 @@ public class PersonaMain {
 	public static void main(String[] args) {
 		System.out.println("THIS IS A USEFULL APP");
 		System.out.println("---------------------");
-		
+	}
+	
+	public static String getPersonIdAndName(){
+		return new Persona("1","Rafael").getName();
 	}
 
 }
