c034e0b506c2dd80b6a36f307f6857bf5d4babdd
cantwait <rafael.cadenas@admios-sa.com>
2014-10-03 14:41:53 -0500

    merged w/ fb2

--- java-git-test/src/org/home/test/AppMain.java
+++ java-git-test/src/org/home/test/AppMain.java
@@ -4,7 +4,9 @@ public class AppMain {
 
 	public static void main(String[] args) {
 		System.out.println("THIS IS A USEFULL APP FOR TESTING");
+		System.out.println("line1");
 		System.out.println("line2");
+		System.out.println("line3");
 	}
 
 }
