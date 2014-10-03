2db5ee2a386b59c5f96c31a50c190baa966eb483
cantwait <rafael.cadenas@admios-sa.com>
2014-10-03 15:29:04 -0500

    first commit

--- java-git-test/src/org/home/test/AppMain.java
+++ java-git-test/src/org/home/test/AppMain.java
@@ -4,9 +4,6 @@ public class AppMain {
 
 	public static void main(String[] args) {
 		System.out.println("THIS IS A USEFULL APP FOR TESTING");
-		System.out.println("line1");
-		System.out.println("line2");
-		System.out.println("line3");
 	}
 
 }
